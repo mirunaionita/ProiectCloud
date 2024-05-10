89dcbc74d6dd3cdaeea8ef5549a9b71d98988782
Cioger <blntheodor@gmail.com>
2024-05-10 20:50:58 +0300

    updated button

--- components/ChatComponent.jsx
+++ components/ChatComponent.jsx
@@ -77,7 +77,7 @@ const ChatComponent = () => {
       <button
             type="button"
             onClick={handleBack}
-            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            className="text-white bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
             Back
       </button>
